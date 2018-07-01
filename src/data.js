990a394ddeea1fe80a34c09dc7b295c7f487aa7e
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-07-01 16:41:35 -0500

    agregando-api

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 window.laboratoria = {};
 window.getData = async () => {
-  const url = 'https://raw.githubusercontent.com/Vicky2813/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
+  const url = 'https://api.myjson.com/bins/u2aqe';
   const promiseData = await fetch(url);
   laboratoria = await promiseData.json();
 }
