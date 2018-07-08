195401ff9b28d6193f13b0f62988ee187b76805d
Victoria Acosta <vicky.acostahidalgo@gmail.com>
2018-07-08 16:49:23 -0500

    Modificando CSS y HTML. Agregando RD

--- src/data.js
+++ src/data.js
@@ -25,16 +25,44 @@ window.computeGenerationsStats = (event) => {
 window.computeStudentsStats = () => {
   console.log(event.target.innerHTML);
   ulStudents.innerHTML = ''
+  tableDataStudents.innerHTML = ''
   containerStudents.style.display = 'block';
-  const generation = event.target.innerHTML;
+  const generation = event.target.innerHTML.toLowerCase();
   const estudiantes = laboratoria[keySede].generacion[generation].estudiantes;
   console.log(estudiantes);
 
-  estudiantes.forEach(estudiante => {
+  estudiantes.forEach((estudiante, index) => {
+    // PARA CREAR LA LISTA
     const liStudent = document.createElement('li')
-    liStudent.innerHTML = `${estudiante.nombre}, ${estudiante.correo}, CAMPUS: ${keySede}, GENERACION: ${generation}`
+    liStudent.innerHTML = `${index + 1} - ${estudiante.nombre} <i class="fas fa-at"></i> ${estudiante.correo}`
     liStudent.className = 'list-group-item'
     ulStudents.appendChild(liStudent)
+
+    // PARA CREAR LA TABLA
+    const tr = document.createElement('tr');
+    const thIndex = document.createElement('th');
+    thIndex.innerHTML = index + 1
+    tr.appendChild(thIndex)
+    const tdName = document.createElement('td');
+    tdName.innerHTML = estudiante.nombre
+    tr.appendChild(tdName)
+    const tdEmail = document.createElement('td');
+    tdEmail.innerHTML = estudiante.correo
+    tr.appendChild(tdEmail)
+    const tdCampus = document.createElement('td');
+    tdCampus.innerHTML = keySede
+    tr.appendChild(tdCampus)
+    const tdGeneration = document.createElement('td');
+    tdGeneration.innerHTML = generation
+    tr.appendChild(tdGeneration)
+    const tdDuration = document.createElement('td');
+    tdDuration.innerHTML = estudiante.progreso.duracionPrograma
+    tr.appendChild(tdDuration)
+    const tdScored = document.createElement('td');
+    tdScored.innerHTML = estudiante.progreso.porcentajeCompletado
+    tr.appendChild(tdScored)
+
+    tableDataStudents.appendChild(tr)
   })
 
 
--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ let generaciones;
 // SEDE INDEX
 let keySede;
 
+// FONDO LABORATORIA
+const backgroundLaboratoria = document.getElementById('background-laboratoria')
+
 // SEDE LIMA
 const headquartersLima = document.getElementById('sede-lima');
 
@@ -27,13 +30,19 @@ const ulStudents = document.getElementById('students');
 
 const containerStudents = document.getElementById('students-container')
 
+// TABLA DE DATOS DE ESTUDIANTE
+let tableDataStudents = document.getElementById('data-students')
+
 // EVENTO PARA SEDE DE LIMA
 headquartersLima.addEventListener('click', (event) => {
   event.preventDefault();//evitar que recargue la pagina
+
+  // APAGAR FONDO
+  backgroundLaboratoria.style.display = 'none'
   ulGeneraciones.innerHTML = '';
   keySede = 'lima';
   containerStudents.style.display = 'none'
-  h2Sede.innerHTML = event.target.text;
+  h2Sede.innerHTML = event.target.text.toUpperCase();
   //console.log(event.target.text);
 
   const generaciones = Object.keys(laboratoria[keySede].generacion);
@@ -41,26 +50,18 @@ headquartersLima.addEventListener('click', (event) => {
 
   generaciones.forEach(e => {
     let li = document.createElement('li');
-    li.innerHTML = `${e}`;//template string
+    li.innerHTML = `${e.toUpperCase()}`
     li.className = 'generation';//se le asigna una class al li
     li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
     ulGeneraciones.appendChild(li);//
   })
-
-  /*const estudiantes = Object.keys(laboratoria[keySede].generacion[generation].estudiantes);
-
-  estudiantes.forEach(estudiantes => {
-    let li = document.createElement('li');
-    li.innerHTML = `${estudiantes}`;
-    li.className = 'students';
-    ulEstudiantes.appendChild(li);
-  })
-  */
 })
 
 // EVENTO PARA SEDE DE CDMX
 headquartersMexico.addEventListener('click', (event) => {
   event.preventDefault();
+  // APAGAR FONDO
+  backgroundLaboratoria.style.display = 'none'
   ulGeneraciones.innerHTML = '';
   keySede = 'mexico';
   console.log(event.target.text);
@@ -71,16 +72,18 @@ headquartersMexico.addEventListener('click', (event) => {
   console.log(generaciones);
 
   generaciones.forEach(e => {
-    let li = document.createElement('li');
-    li.innerHTML = `${e}`;
-    li.className = 'generation';
-    li.onclick = computeStudentsStats;
-    ulGeneraciones.appendChild(li);
+    let li = document.createElement('li')
+    li.innerHTML = `${e.toUpperCase()}`
+    li.className = 'generation';//se le asigna una class al li
+    li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
+    ulGeneraciones.appendChild(li);//
   })
 })
 // EVENTO PARA SEDE DE SANTIAGO
 headquartersSantiago.addEventListener('click', (event) => {
   event.preventDefault();
+  // APAGAR FONDO
+  backgroundLaboratoria.style.display = 'none'
   ulGeneraciones.innerHTML = '';
   keySede = 'santiago';
   console.log(event.target.text);
@@ -90,10 +93,10 @@ headquartersSantiago.addEventListener('click', (event) => {
   console.log(generaciones);
 
   generaciones.forEach(e => {
-    let li = document.createElement('li');
-    li.innerHTML = `${e}`;
-    li.className = 'generation';
-    li.onclick = computeStudentsStats;
-    ulGeneraciones.appendChild(li);
+    let li = document.createElement('li')
+    li.innerHTML = `${e.toUpperCase()}`
+    li.className = 'generation';//se le asigna una class al li
+    li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
+    ulGeneraciones.appendChild(li);//
   })
 })
