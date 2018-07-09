c8046712adf8c3855526cd897410d524b8d1ebdc
Victoria Acosta <vicky.acostahidalgo@gmail.com>
2018-07-09 09:38:31 -0500

    actualizando guia de estilos

--- src/data.js
+++ src/data.js
@@ -1,21 +1,22 @@
-//ASEGURAR QUE AL CARGAR LA PAGINA TENGA LOS DATOS DE INICION
-document.addEventListener('DOMContentLoaded', ()=>{//DOMContentLoaded es un evento
+// ASEGURAR QUE AL CARGAR LA PAGINA TENGA LOS DATOS DE INICION
+// DOMContentLoaded es un evento
+document.addEventListener('DOMContentLoaded', ()=>{
   getData();
-},false)
+}, false);
 
 window.laboratoria = {};
 window.getData = () => {
   const url = 'https://api.myjson.com/bins/u2aqe';
   fetch(url)
-  .then(promiseData => promiseData.json())
-  .then (data => {
-    laboratoria = data;
-    //OBTENGO EL ARRAY DE KEYS CORRESPONDIENTE A LAS SEDES
-    headquarters = Object.keys(laboratoria);
-  })
-  //TEXTO QUE IMPRIME EL ERROR
-  .catch(err => console.log("Error", err.message))
-}
+    .then(promiseData => promiseData.json())
+    .then(data => {
+      laboratoria = data;
+      // OBTENGO EL ARRAY DE KEYS CORRESPONDIENTE A LAS SEDES
+      headquarters = Object.keys(laboratoria);
+    })
+  // TEXTO QUE IMPRIME EL ERROR
+    .catch(err => console.log('Error', err.message));
+};
 
 window.computeGenerationsStats = (event) => {
 
@@ -24,8 +25,8 @@ window.computeGenerationsStats = (event) => {
 
 window.computeStudentsStats = () => {
   console.log(event.target.innerHTML);
-  ulStudents.innerHTML = ''
-  tableDataStudents.innerHTML = ''
+  ulStudents.innerHTML = '';
+  tableDataStudents.innerHTML = '';
   containerStudents.style.display = 'block';
   const generation = event.target.innerHTML.toLowerCase();
   const estudiantes = laboratoria[keySede].generacion[generation].estudiantes;
@@ -33,39 +34,37 @@ window.computeStudentsStats = () => {
 
   estudiantes.forEach((estudiante, index) => {
     // PARA CREAR LA LISTA
-    const liStudent = document.createElement('li')
-    liStudent.innerHTML = `${index + 1} - ${estudiante.nombre} <i class="fas fa-at"></i> ${estudiante.correo}`
-    liStudent.className = 'list-group-item'
-    ulStudents.appendChild(liStudent)
+    const liStudent = document.createElement('li');
+    liStudent.innerHTML = `${index + 1} - ${estudiante.nombre} <i class="fas fa-at"></i> ${estudiante.correo}`;
+    liStudent.className = 'list-group-item';
+    ulStudents.appendChild(liStudent);
 
     // PARA CREAR LA TABLA
     const tr = document.createElement('tr');
     const thIndex = document.createElement('th');
-    thIndex.innerHTML = index + 1
-    tr.appendChild(thIndex)
+    thIndex.innerHTML = index + 1;
+    tr.appendChild(thIndex);
     const tdName = document.createElement('td');
-    tdName.innerHTML = estudiante.nombre
-    tr.appendChild(tdName)
+    tdName.innerHTML = estudiante.nombre;
+    tr.appendChild(tdName);
     const tdEmail = document.createElement('td');
-    tdEmail.innerHTML = estudiante.correo
-    tr.appendChild(tdEmail)
+    tdEmail.innerHTML = estudiante.correo;
+    tr.appendChild(tdEmail);
     const tdCampus = document.createElement('td');
-    tdCampus.innerHTML = keySede
-    tr.appendChild(tdCampus)
+    tdCampus.innerHTML = keySede;
+    tr.appendChild(tdCampus);
     const tdGeneration = document.createElement('td');
-    tdGeneration.innerHTML = generation
-    tr.appendChild(tdGeneration)
+    tdGeneration.innerHTML = generation;
+    tr.appendChild(tdGeneration);
     const tdDuration = document.createElement('td');
-    tdDuration.innerHTML = estudiante.progreso.duracionPrograma
-    tr.appendChild(tdDuration)
+    tdDuration.innerHTML = estudiante.progreso.duracionPrograma;
+    tr.appendChild(tdDuration);
     const tdScored = document.createElement('td');
-    tdScored.innerHTML = estudiante.progreso.porcentajeCompletado
-    tr.appendChild(tdScored)
+    tdScored.innerHTML = estudiante.progreso.porcentajeCompletado;
+    tr.appendChild(tdScored);
 
-    tableDataStudents.appendChild(tr)
-  })
-
-
+    tableDataStudents.appendChild(tr);
+  });
 };
 
 window.sortStudents = () => {};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // LISTA DE SEDES
 let headquarters;
 
-//GENERACIONES
+// GENERACIONES
 let generaciones;
 
 // SEDE INDEX
 let keySede;
 
 // FONDO LABORATORIA
-const backgroundLaboratoria = document.getElementById('background-laboratoria')
+const backgroundLaboratoria = document.getElementById('background-laboratoria');
 
 // SEDE LIMA
 const headquartersLima = document.getElementById('sede-lima');
@@ -25,78 +25,78 @@ const h2Sede = document.getElementById('sede');
 // REFERENCIA AL ELEMENTO GENERACIONES
 const ulGeneraciones = document.getElementById('generations');
 
-//REFERENCIA AL ELEMENTO ESTUDIANTE
+// REFERENCIA AL ELEMENTO ESTUDIANTE
 const ulStudents = document.getElementById('students');
 
-const containerStudents = document.getElementById('students-container')
+const containerStudents = document.getElementById('students-container');
 
 // TABLA DE DATOS DE ESTUDIANTE
-let tableDataStudents = document.getElementById('data-students')
+let tableDataStudents = document.getElementById('data-students');
 
 // EVENTO PARA SEDE DE LIMA
 headquartersLima.addEventListener('click', (event) => {
-  event.preventDefault();//evitar que recargue la pagina
+  event.preventDefault();// evitar que recargue la pagina
 
   // APAGAR FONDO
-  backgroundLaboratoria.style.display = 'none'
+  backgroundLaboratoria.style.display = 'none';
   ulGeneraciones.innerHTML = '';
   keySede = 'lima';
-  containerStudents.style.display = 'none'
+  containerStudents.style.display = 'none';
   h2Sede.innerHTML = event.target.text.toUpperCase();
-  //console.log(event.target.text);
+  // console.log(event.target.text);
 
   const generaciones = Object.keys(laboratoria[keySede].generacion);
   console.log(generaciones);
 
-  generaciones.forEach(e => {
+  generaciones.forEach(event => {
     let li = document.createElement('li');
-    li.innerHTML = `${e.toUpperCase()}`
-    li.className = 'generation';//se le asigna una class al li
-    li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
+    li.innerHTML = `${event.toUpperCase()}`;
+    li.className = 'generation';// se le asigna una class al li
+    li.onclick = computeStudentsStats;// cuando das click se ejecute esta funcion
     ulGeneraciones.appendChild(li);//
-  })
-})
+  });
+});
 
 // EVENTO PARA SEDE DE CDMX
 headquartersMexico.addEventListener('click', (event) => {
   event.preventDefault();
   // APAGAR FONDO
-  backgroundLaboratoria.style.display = 'none'
+  backgroundLaboratoria.style.display = 'none';
   ulGeneraciones.innerHTML = '';
   keySede = 'mexico';
   console.log(event.target.text);
 
-  h2Sede.innerHTML = event.target.text
-  containerStudents.style.display = 'none'
+  h2Sede.innerHTML = event.target.text;
+  containerStudents.style.display = 'none';
   const generaciones = Object.keys(laboratoria[keySede].generacion);
   console.log(generaciones);
 
-  generaciones.forEach(e => {
-    let li = document.createElement('li')
-    li.innerHTML = `${e.toUpperCase()}`
-    li.className = 'generation';//se le asigna una class al li
-    li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
+  generaciones.forEach(event => {
+    let li = document.createElement('li');
+    li.innerHTML = `${event.toUpperCase()}`;
+    li.className = 'generation';// se le asigna una class al li
+    li.onclick = computeStudentsStats;// cuando das click se ejecute esta funcion
     ulGeneraciones.appendChild(li);//
-  })
-})
+  });
+});
 // EVENTO PARA SEDE DE SANTIAGO
 headquartersSantiago.addEventListener('click', (event) => {
   event.preventDefault();
   // APAGAR FONDO
-  backgroundLaboratoria.style.display = 'none'
+  backgroundLaboratoria.style.display = 'none';
   ulGeneraciones.innerHTML = '';
   keySede = 'santiago';
   console.log(event.target.text);
-  containerStudents.style.display = 'none'
+  containerStudents.style.display = 'none';
   h2Sede.innerHTML = event.target.text;
   const generaciones = Object.keys(laboratoria[keySede].generacion);
   console.log(generaciones);
 
-  generaciones.forEach(e => {
-    let li = document.createElement('li')
-    li.innerHTML = `${e.toUpperCase()}`
-    li.className = 'generation';//se le asigna una class al li
-    li.onclick = computeStudentsStats;//cuando das click se ejecute esta funcion
+  generaciones.forEach(event => {
+    let li = document.createElement('li');
+    li.innerHTML = `${event.toUpperCase()}`;
+    li.className = 'generation';// se le asigna una class al li
+    li.onclick = computeStudentsStats;// cuando das click se ejecute esta funcion
     ulGeneraciones.appendChild(li);//
-  })
-})
+  });
+});
